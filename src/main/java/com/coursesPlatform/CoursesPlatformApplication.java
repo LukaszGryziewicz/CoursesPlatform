c95cef6792caacdc7c2bb42aa4f56693d163cd29
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-06-12 09:48:46 +0200

    initial commit

---
+++ src/main/java/com/coursesPlatform/CoursesPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.coursesPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesPlatformApplication.class, args);
+	}
+
+}
