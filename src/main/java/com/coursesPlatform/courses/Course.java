c93e1d672b6cfee1db926d68607562b50ca39c08
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-06-12 11:15:05 +0200

    added Services

---
+++ src/main/java/com/coursesPlatform/courses/Course.java
@@ -0,0 +1,59 @@
+package com.coursesPlatform.courses;
+
+import com.coursesPlatform.category.Category;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.HashMap;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private Category category;
+
+
+    public Course(String title, String description, Category category) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+}
