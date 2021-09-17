25109d841db9a69d52f5b46c8156c786c069baf9
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-09-18 01:57:29 +0200

    added transactional to delete method

--- src/main/java/com/coursesPlatform/trainer/TrainerRepository.java
+++ src/main/java/com/coursesPlatform/trainer/TrainerRepository.java
@@ -2,10 +2,12 @@ package com.coursesPlatform.trainer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 interface TrainerRepository extends JpaRepository<Trainer, Long> {
     Optional<Trainer> findByNameAndLastName(String name, String lastName);
 
+    @Transactional
     void deleteByNameAndLastName(String name, String lastName);
 }
